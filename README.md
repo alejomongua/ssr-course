952d987337f6315e6d9c91a21935de9c82757788
Alejandro Mongua <alejandromongua@tecnimaq.com>
2020-08-06 18:56:17 -0500

    Commit inicial, finalizada la sección 3

---
+++ README.md
@@ -0,0 +1,7 @@
+# Repositorio del curso de SSR
+
+Curso de Udemy
+
+https://www.udemy.com/course/server-side-rendering-with-react-and-redux
+
+
---
+++ src/client/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      Hello cruel world
+    </div>
+  )
+}
+
+Home.propTypes = {
+
+}
+
+export default Home
+
