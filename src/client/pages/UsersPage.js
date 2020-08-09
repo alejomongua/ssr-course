03e470f4da943d3aca588d249a074f0f1e976144
Alejandro Mongua <alejandromongua@gmail.com>
2020-08-08 22:50:04 -0500

    Added og headers with Helmeth

--- src/client/pages/UsersPage.js
+++ src/client/pages/UsersPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchUsers } from '../actions'
 import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
 
 class UsersList extends Component {
   componentDidMount() {
@@ -14,10 +15,20 @@ class UsersList extends Component {
     ))
   }
 
+  head () {
+    return (
+      <Helmet>
+        <title>{`${this.props.users.length} users loaded`}</title>
+        <meta property='og:title' content='SSR cool app' />
+      </Helmet>
+    )
+  }
+
   render() {
     return (
       <div>
-        Lista de usuarios
+        {this.head()}
+        <h1>Lista de usuarios</h1>
         <ul>{this.renderUsers()}</ul>
       </div>
     )
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -5,6 +5,7 @@ import routes from '../client/routes'
 import { StaticRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import serialize from 'serialize-javascript'
+import { Helmet } from 'react-helmet'
 
 export default (req, store, context) => {
   const content = renderToString(
@@ -15,10 +16,13 @@ export default (req, store, context) => {
     </Provider>
   )
   
+  const helmet = Helmet.renderStatic()
+
   return `
     <html>
       <head>
-        <title>Template</title>
+        ${helmet.title.toString()}
+        ${helmet.meta.toString()}
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
       </head>
       <body>
